37d5ce1bde476abe0106fe8c2f27d9be06e6e39c
Felipe Batista <felipe09_2007@hotmail.com>
2025-11-19 20:56:31 +0000

    mofifica o texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>🌻 O meu pensamento tem a cor de seu vestido, ou um girassol que tem a cor de seu cabelo? 🌻</h1>
+    return <h1>🌻 O meu pensamento tem a cor de seu vestido 🌻<br />🌻 Ou um girassol que tem a cor de seu cabelo? 🌻</h1>
 }
 
 export default Home;

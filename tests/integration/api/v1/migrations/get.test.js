6443fb9698981aa3e2840d0ab789da354261faba
Felipe Batista <felipe09_2007@hotmail.com>
2026-02-02 04:18:57 +0000

    refactor: use `next-connect` in `/migrations` and `/status`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -9,12 +9,9 @@ describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
-      // status code == 200 OK
       expect(response.status).toBe(200);
       const responseBody = await response.json();
-      // expect an array in response
       expect(Array.isArray(responseBody)).toBe(true);
-      // expect an array not empty in response
       expect(responseBody.length).toBeGreaterThan(0);
     });
   });

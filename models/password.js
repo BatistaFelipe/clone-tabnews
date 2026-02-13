111b18deea64a3ee5ce1599554f749f074f88132
Felipe Batista <felipe09_2007@hotmail.com>
2026-02-13 01:27:50 +0000

    feat: add `pepper` in password

--- models/password.js
+++ models/password.js
@@ -2,15 +2,13 @@ import bcryptjs from "bcryptjs";
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
-}
-
-function getNumberOfRounds() {
-  return process.env.NODE_ENV === "production" ? 14 : 1;
+  const pepper = getPepper();
+  return await bcryptjs.hash(password + pepper, rounds);
 }
 
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  const pepper = getPepper();
+  return await bcryptjs.compare(providedPassword + pepper, storedPassword);
 }
 
 const password = {
@@ -19,3 +17,11 @@ const password = {
 };
 
 export default password;
+
+function getNumberOfRounds() {
+  return process.env.NODE_ENV === "production" ? 14 : 1;
+}
+
+function getPepper() {
+  return process.env.PASSWORD_PEPPER || "";
+}

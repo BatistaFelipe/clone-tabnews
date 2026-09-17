bef7e9187a38f0a05aa5ce79fb666bb1e4c3e795
Felipe Marta Batista <felipe09_2007@hotmail.com>
2026-09-17 21:20:35 +0000

    feat: require `update:user` to access `/api/v1/users/[username]`

--- models/activation.js
+++ models/activation.js
@@ -114,6 +114,7 @@ async function activateUserByUserId(userId) {
   const activateUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
+    "update:user",
   ]);
   return activateUser;
 }
